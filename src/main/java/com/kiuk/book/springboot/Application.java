3b7ab1ee3c82b5428f3bfa7b0caecee7b6935eda
KiUkKim <zidh1@korea.ac.kr>
2022-01-25 15:44:31 +0900

    Spring Boot course 3 commit

--- src/main/java/com/kiuk/book/springboot/Application.java
+++ src/main/java/com/kiuk/book/springboot/Application.java
@@ -3,7 +3,9 @@ package com.kiuk.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //Jpd의 Auditing Annotation 적용
 @ComponentScan(basePackages = "com.kiuk.book.springboot.service")
 @SpringBootApplication
 public class  Application {
